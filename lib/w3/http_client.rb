aedc67c1698aed0cebcb3ac5e615bcccb96b905c
Maxim Dorofienko <dorofienko@gmail.com>
2018-02-15 15:04:46 +0300

    Basic auth support for W3::Http_Client

--- lib/w3/http_client.rb
+++ lib/w3/http_client.rb
@@ -3,18 +3,21 @@ require 'net/http'
 module W3
   class Http_Client
     attr_accessor :uri, :host, :port
-  
-    def initialize(host)
+
+    def initialize(host, username: nil, password: nil)
       @uri = URI.parse(host)
       raise ArgumentError unless ['http', 'https'].include? uri.scheme
       @host = uri.host
       @port = uri.port
+      @username = username
+      @password = password
     end
-  
+
     def send(payload)
       http = ::Net::HTTP.new(@host, @port)
       header = {'Content-Type' => 'application/json'}
       request = ::Net::HTTP::Post.new(uri, header)
+      request.basic_auth(@username, @password) if @username && @password
       request.body = payload
       response = http.request(request)
       JSON.parse(response.body)
